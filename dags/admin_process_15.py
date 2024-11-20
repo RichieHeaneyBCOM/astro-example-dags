ba437b507a9f3fa306c1b1e6cd283fb770bc289e
RichieHeaneyBCOM <richard.heaney@broadcom.com>
2024-11-20 09:04:13 +0000

    Added in the other test admin DAGs

---
+++ dags/admin_process_15.py
@@ -0,0 +1,27 @@
+# Creating a DAG that runs a different sleep time
+# Using this for chainging multiple DAGs to emulate a process
+
+from time import sleep
+from datetime import datetime
+from random import randint
+import time
+
+import sys
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+
+def admin_process_15():
+    timer = randint(1, 600)
+    for i in reversed(range(timer)):
+        sys.stderr.write(f'\rsleeping for {i} seconds')
+        sleep(1)
+
+dag = DAG('admin_process_15', description='Admin Processing Step 15',
+          schedule_interval='0 12 * * *',
+          start_date=datetime(2017, 3, 20), catchup=False)
+
+admin_process_15_operator = PythonOperator(task_id='admin_process_15_task', python_callable=admin_process_15, dag=dag)
+
+admin_process_15_operator
